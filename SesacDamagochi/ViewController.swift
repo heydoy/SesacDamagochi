f249c7765ba636150695722ead6ae4d142b68497
heydoy <heydoyhey@gmail.com>
2022-07-23 22:49:12 +0900

    Lottie를 이용한 Launch Screen

--- SesacDamagochi/ViewController.swift
+++ SesacDamagochi/ViewController.swift
@@ -6,12 +6,28 @@
 //
 
 import UIKit
+import Lottie
+
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var viewForLottie: AnimationView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+
+        
+        // animationView
+        viewForLottie.frame = self.view.bounds
+        viewForLottie.center = self.view.center
+        viewForLottie.contentMode = .scaleAspectFit
+        viewForLottie.loopMode = .repeatBackwards(1)
+        viewForLottie.animationSpeed = 0.9
+        
+        viewForLottie.play { (finished) in
+            print("애니메이션 끝")
+        }
     }
 
 
