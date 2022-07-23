c05789c1ee2c5668ab82ed29fa96d8781e1a1754
heydoy <heydoyhey@gmail.com>
2022-07-23 21:34:20 +0900

    커스텀 색상(배경, 폰트/테투리) UIColor Extension으로 지정

---
+++ SesacDamagochi/Extension/UIColor+Extension.swift
@@ -0,0 +1,26 @@
+//
+//  UIColor+Extension.swift
+//  SesacDamagochi
+//
+//  Created by Doy Kim on 2022/07/23.
+//
+
+import UIKit
+
+extension UIColor {
+    //static let DamagochiBackgroundColor = UIColor(named: "DamagochiBackgroundColor")
+    
+    static let DamagochiBackgroundColor = UIColor(
+        red: 245/255,
+        green: 252/255,
+        blue: 252/255,
+        alpha: 1.0
+    )
+    
+    static let DamagochiFontAndBorderColor = UIColor(
+        red: 77/255,
+        green: 106/255,
+        blue: 120/255,
+        alpha: 1.0
+    )
+}
